911ca4d1bb9d9ea99b3b681f0621419931aa59da
Kassio Schaider <kassio.schaider@basis.com.br>
2019-12-01 14:53:13 -0300

    first commit

---
+++ src/main/java/br/com/basis/kassioschaider/desafioangularspring/DesafioAngularSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.basis.kassioschaider.desafioangularspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAngularSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/basis/kassioschaider/desafioangularspring/DesafioAngularSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.basis.kassioschaider.desafioangularspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAngularSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
