9ff6c5e9ce86ededc1c9256ebd557d153bec3e7f
Kassio Schaider <kassio.schaider@basis.com.br>
2019-12-03 14:47:34 -0300

    Primeiro commit Cliente-Web

--- desafio-front/src/app/app.component.spec.ts
+++ desafio-front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'desafio'`, () => {
+  it(`should have as title 'desafio-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('desafio');
+    expect(app.title).toEqual('desafio-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('desafio app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('desafio-front app is running!');
   });
 });
